76efdd059aa12888b37d8c6cb85af6e65dc4d236
Jason Edgecombe <jason@rampaginggeek.com>
2021-03-10 19:32:42 -0500

    BASH: Add fzf support

---
+++ .bashrc.d/fzf.sh
@@ -0,0 +1,17 @@
+# Setup fzf
+# ---------
+if [[ -d "${HOME}/.fzf" ]]; then
+
+  if [[ ! "$PATH" == *"${HOME}/.fzf/bin"* ]]; then
+    export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.bash" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "${HOME}/.fzf/shell/key-bindings.bash"
+
+fi
