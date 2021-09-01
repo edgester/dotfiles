7b6834167f92d18227036033bb81576ae35812f9
Jason Edgecombe <jason@rampaginggeek.com>
2021-08-31 22:42:35 -0400

    BASH: Tweak fzf settings

--- .bashrc.d/fzf-settings.sh
+++ .bashrc.d/fzf-settings.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export FZF_DEFAULT_OPTS='--multi --border --preview="head -20 {+}" --preview-window=down'
+export FZF_DEFAULT_OPTS='--multi --cycle --preview="head -n${FZF_PREVIEW_LINES} {+}"'
