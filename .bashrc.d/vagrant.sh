cd61091e7669bbb33fcba224b8f895390b413e65
Jason Edgecombe <jason@rampaginggeek.com>
2022-03-14 13:11:11 -0400

    BASH: Add vagrant-libvirt function

--- .bashrc.d/vagrant.sh
+++ .bashrc.d/vagrant.sh
@@ -3,20 +3,36 @@
 # Use vagrant container on RHEL8
 if [ "${PLATFORM_ID}" == "platform:el8" ]; then
 
+    vagrant-libvirt(){
+        podman run -it --rm \
+            -e LIBVIRT_DEFAULT_URI \
+            -v /var/run/libvirt/:/var/run/libvirt/ \
+            -v ~/.vagrant.d/boxes:/vagrant/boxes \
+            -v ~/.vagrant.d/data:/vagrant/data \
+            -v ~/.vagrant.d/tmp:/vagrant/tmp \
+            -v $(realpath "${PWD}"):${PWD} \
+            -w $(realpath "${PWD}") \
+            --network host \
+            --entrypoint /bin/bash \
+            --security-opt label=disable \
+            docker.io/vagrantlibvirt/vagrant-libvirt:latest \
+            vagrant $@
+    }
+
     vagrant(){
-      podman run -it --rm \
-        -e LIBVIRT_DEFAULT_URI \
-        -v /var/run/libvirt/:/var/run/libvirt/ \
-        -v ~/.vagrant.d/boxes:/vagrant/boxes \
-        -v ~/.vagrant.d/data:/vagrant/data \
-        -v ~/.vagrant.d/tmp:/vagrant/tmp \
-        -v $(realpath "${PWD}"):${PWD} \
-        -w $(realpath "${PWD}") \
-        --network host \
-        --entrypoint /bin/bash \
-        --security-opt label=disable \
-        docker.io/vagrantlibvirt/vagrant-libvirt:latest \
-          vagrant $@
+        podman run -it --rm \
+            -e LIBVIRT_DEFAULT_URI \
+            -v /var/run/libvirt/:/var/run/libvirt/ \
+            -v ~/.vagrant.d/boxes:/vagrant/boxes \
+            -v ~/.vagrant.d/data:/vagrant/data \
+            -v ~/.vagrant.d/tmp:/vagrant/tmp \
+            -v $(realpath "${PWD}"):${PWD} \
+            -w $(realpath "${PWD}") \
+            --network host \
+            --entrypoint /bin/bash \
+            --security-opt label=disable \
+            localhost/vagrant-extras \
+            vagrant $@
     }
 
 fi
