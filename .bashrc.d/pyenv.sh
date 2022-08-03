4020bb8cd62c73c733bad9e43fae2ad4421c1397
Jason Edgecombe <jason@rampaginggeek.com>
2022-08-02 20:55:19 -0400

    Add pyenv

---
+++ .bashrc.d/pyenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Load pyenv if it's present
+if [ -d "${HOME}/.pyenv" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+fi
