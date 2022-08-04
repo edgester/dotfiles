64701f9bcb4765658df18b53e38c88fe60698c19
Jason Edgecombe <jason@rampaginggeek.com>
2022-08-03 20:40:19 -0400

    BASH: pyenv: enable auto-activation of virtualenv's

--- .bashrc.d/pyenv.sh
+++ .bashrc.d/pyenv.sh
@@ -4,4 +4,5 @@ if [ -d "${HOME}/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
 fi
