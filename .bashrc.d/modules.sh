77b61f68c98a2ee93048a7470d03fcc7363e1333
Jason Edgecombe <jason@rampaginggeek.com>
2022-09-13 22:49:59 -0400

    BASH: Source modules if present

--- .bashrc.d/modules.sh
+++ .bashrc.d/modules.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Load personal modules if the module command is available
+[ -f /etc/profile.d/modules.sh ] && . /etc/profile.d/modules.sh
 if [[ $(type -t module) == function ]]; then
     module use --append ~/modules/
 fi
