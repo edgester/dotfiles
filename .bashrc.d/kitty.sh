ba627641c468e832e199fb9d24507ca4d10ae651
Jason Edgecombe <jason@rampaginggeek.com>
2022-09-14 16:11:11 -0400

    BASH: Set TERM to fix vim colors
    Pretend to be gnome-terminal when using kitty terminal

--- .bashrc.d/kitty.sh
+++ .bashrc.d/kitty.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Make vim and all the other programs work correctly in kitty
+[ "$TERM" == "xterm-kitty" ] && export TERM="xterm-256color"
+
 alias icat="kitty +kitten icat --align=left"
